9a99147d9aafdd7ae17b1d9020449e97cb683ddf
RaulMirandaOSF <raul.miranda@osf-commerce.com>
2019-03-11 16:00:15 -0300

    Done ”T-Shirt Info” section

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
 $(document).ready(function(){
 
+    const jspunsOptions = ["cornflowerblue", "darkslategrey", "gold"];
+    const heartjsOptions = ["tomato", "steelblue", "dimgrey"];
+
     // Setup page
 
     $('#name').focus();
     $('#other-title').hide();
+    hideColorOptions();
 
     // Show or Hide other job role input
-    
+
     $('#title').change(function() {
         if ($(this).val() == 'other') {
             $('#other-title').show();
@@ -14,4 +18,44 @@ $(document).ready(function(){
             $('#other-title').hide();
         }
     });
+
+
+    // Configure color options dependent of Design
+
+    $('#design').change(function() {
+
+        $('#color').show();
+        $('#colors-js-puns label').show();
+
+        if ($(this).val() == "js puns") {
+            
+            $.each(jspunsOptions, function(index, value) {
+                $('#color option[value='+ value +']').show();
+            });
+            $.each(heartjsOptions, function(index, value) {
+                $('#color option[value='+ value +']').hide();
+            });
+
+            $('#color').val(jspunsOptions[0]);
+
+        } else if ($(this).val() == 'heart js') {
+            
+            $.each(jspunsOptions, function(index, value) {
+                $('#color option[value='+ value +']').hide();
+            });
+            $.each(heartjsOptions, function(index, value) {
+                $('#color option[value='+ value +']').show();
+            });
+
+            $('#color').val(heartjsOptions[0]);
+
+        } else {
+            hideColorOptions();
+        }
+    });
+
+    function hideColorOptions() {
+        $('#colors-js-puns label').hide();
+        $('#color').hide();
+    }
 });
